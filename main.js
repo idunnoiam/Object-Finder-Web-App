2b051f2ad411f335bddf200546b26541b1189491
idunnoiam <74386219+idunnoiam@users.noreply.github.com>
2022-04-09 20:58:02 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 status = "";
 objects = [];
+video = "";
 
 function preload() {
 
@@ -15,20 +16,55 @@ function setup() {
 
 }
 
-function draw() {
-    image(video, 0, 0, 380, 380);
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
 }
 
 function start() {
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML = "Status : Detecting Objects";
-    object = document.getElementById("ID_Of_The_Input_Box").ariaValueMax;
 }
+
+function draw() {
+    image(video, 0, 0, 380, 380);
+
+    if (status != "") {
+        objectDetector.detect(video, gotResult);
+
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Objects Detected";
+            document.getElementById("number_of_objects").innerHTML = "Number of objects are : " + objects.length;
+            object = document.getElementById("input_box").value;
+
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+            if (objects[i].label = object) {
+                object = document.getElementById("input_box").value;
+                video.stop()
+                document.getElementById("number_of_objects").innerHTML = object + " Found";
+                var synth = window.speechSynthesis;
+                var utterThis = new SpeechSynthesisUtterance(object + " has been found");
+                synth.speak(utterThis);
+            } else {
+                console.log("Mission Failed");
+            }
+
+        }
+
+    }
+}
+
 
 function modelLoaded() {
     console.log("Model Loaded")
     status = true;
-    video.loop();
-    video.speed(1);
-    video.volume(0);
 }
